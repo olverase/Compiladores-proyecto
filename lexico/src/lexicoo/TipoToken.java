0ce751fcebf77e89211e19def884ae10d8f17ddb
Emiliano <107131046+olverase@users.noreply.github.com>
2023-10-09 22:56:43 -0600

    Add files via upload

---
+++ lexico/src/lexicoo/TipoToken.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lexicoo;
+
+
+public enum TipoToken {
+    // Crear un tipoToken por palabra reservada
+    // Crear un tipoToken: identificador, una cadena y numero
+    // Crear un tipoToken por cada "Signo del lenguaje" (ver clase Scanner)
+
+
+    // Palabras clave:
+     Y, CLASS, IF, ADICIONAL, FALSE, FOR, FUN, NULL, O, PRINTLN, RETURN, INT,MAIN,SWITCH,
+    SUPER, THIS, TRUE, VAR, WHILE,ELSE, PARENTESIS_IZQUIERDO,PARENTESIS_DERECHO,CORCHETE_IZQUIERDO,CORCHETE_DERECHO,LLAVE_IZQUIERDA,
+    LLAVE_DERECHA,COMA,PUNTO,PUNTO_Y_COMA,SUMA,RESTA,MULTIPLICACION,DIVISION,NEGACION,DIFERENTE,ASIGNAR,IGUAL_A,MENOR_QUE,MENOR_O_IGUAL,MAYOR_QUE,
+    MAYOR_O_IGUAL,IDENTIFICADOR,CADENA,NUMERO,PALABRA_RESERVADA,INICIO,INTERMEDIO,FIN,ERROR,
+
+    // Final de cadena
+    EOF
+}
---
+++ lexico/src/lexicoo/Token.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lexicoo;
+
+
+class Token {
+    final TipoToken tipo;
+    final String lexema;
+    final Object literal;
+    final int linea;
+
+    public Token(TipoToken tipo, String lexema, Object literal, int linea) {
+        this.tipo = tipo;
+        this.lexema = lexema;
+        this.literal = literal;
+        this.linea = linea;
+        
+        
+    }
+    
+    
+
+    public String toString(){
+        return tipo + " " + lexema + " " + literal;
+    }
+}
